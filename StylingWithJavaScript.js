020822c25fbd46bf4175680895c2306970700607
bradkerwin <bkerwin101@gmail.com>
2024-08-13 15:19:32 -0400

    new html and js added

---
+++ StylingWithJavaScript.js
@@ -0,0 +1,6 @@
+function styleEffect() {
+    const container = document.getElementById("container")
+    container.classList.add("highlighted");
+    container.style.color = "blue"
+    container.style.backgroundColor = "yellow";
+}
