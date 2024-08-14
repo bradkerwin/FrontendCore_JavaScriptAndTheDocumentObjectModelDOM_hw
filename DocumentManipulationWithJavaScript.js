d7d516d88a61ad6158d53e5bfc522bee5a1ef1a8
bradkerwin <bkerwin101@gmail.com>
2024-08-14 12:56:22 -0400

    Document Manipulation files fixed

--- DocumentManipulationWithJavaScript.js
+++ DocumentManipulationWithJavaScript.js
@@ -2,8 +2,16 @@ let products = [
     {name: "cd", price: 10.00, description: "Listen to your favorite music"},
     {name: "stereo", price: 700.50, description: "Top tier sound quality for all your favorites"},
     {name: "soundboard", price: 300.00, description: "Create and download all your favorite sounds and sound effects"}
-];
+    ];
 
-function displayCatalog() {
-    const message = document.getElementById("product-catalog").addEventListener(products);
-}
+    function displayCatalog() {
+        const message = document.getElementById("product-catalog");
+        let productDetails = "<ul>"
+        products.forEach(product => {
+            productDetails += `<li>${product.name}, ${product.price}, ${product.description}</li>`
+        });
+        productDetails += "</ul>"
+        message.innerHTML = productDetails
+    }
+
+    document.getElementById("view-catalog").addEventListener("click", displayCatalog());
